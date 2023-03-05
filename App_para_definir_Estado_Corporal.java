0b68582fe5c86b05c7ca2a3ff8cfc9cb8bc332ca
Daniela <126025562+Daniela1214@users.noreply.github.com>
2023-03-04 22:20:21 -0500

    Add files via upload

---
+++ App_para_definir_Estado_Corporal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App_para_definir_Estado_Corporal {
+    
+    public static void main (String [] args) {
+       Scanner entrada = new Scanner (System.in);
+       System.out.println ("Insertar peso");
+       int peso = entrada.nextInt();
+        if (peso >= 85){
+            System.out.println("Estas Gordo");
+        }
+        else if (peso<85 && peso>=70){
+            System.out.println("Estas Normal");
+        }
+        else if (peso<70 && peso>=55){
+            System.out.println("Estas Delgado");
+        }
+        else{
+            System.out.println("Estas Muy Delgado");
+        }
+    }
+}
